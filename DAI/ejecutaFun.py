3b4f31147b8ba985eaf887335db824e39b70e758
dai <dai@itam.mx>
2019-11-21 12:53:38 -0600

    aqsd

---
+++ DAI/ejecutaFun.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 11:22:42 2019
+
+@author: dai
+"""
+
+import ejemFunciones as f
+
+print(f.suma(5,3))
+
+print(f.suma(a=5,b=3))
+
+print(f.suma())
+
+print(f.suma(5))
+
+print(f.suma(b=15))
+
+print(list(f.circulo(10)))
+t=list(f.circulo(10))
+print(type(t))
+print(f.saludar('fr'))
